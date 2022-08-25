d3e8b9ea9e4611d3db614ed04268cba94b61b95d
Marquis <smcneal89@gmail.com>
2022-08-25 16:38:45 -0400

    updated app.js src with import routes:

--- react-portfolio/react-portfolio/src/App.js
+++ react-portfolio/react-portfolio/src/App.js
@@ -1,25 +1,29 @@
-// import logo from './logo.svg';
-// import './App.css';
+import React from 'react'
+import Header from './components/header/header'
+import Footer from './components/footer/footer'
+import Nav from './components/nav/nav'
+import About from './compAbout fromav/nav'
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+import Experience from './components/experience/experience'
+import Portfolio from './components/portfolio/portfolio'
+import Contact from './components/contact/contact'
 
-// export default App;
+
+const App = () => {
+  return(
+    <>
+    <Header/>
+    <Nav/>
+    <About/>
+    <Experience/>
+    
+    {/* Services will be added for 2.0 release */}
+    {/* <Services/> */}
+      
+    <Portfolio/>
+    <Contact/>
+    <Footer/>
+       
+    </>
+  )
+}
