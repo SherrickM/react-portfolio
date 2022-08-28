07c86b01d138a4a07bc1966b9b074b36b8cfd61a
Marquis <smcneal89@gmail.com>
2022-08-28 14:37:18 -0400

    added nav icons, update nav jsx

--- react-portfolio/react-portfolio/src/components/nav/nav.jsx
+++ react-portfolio/react-portfolio/src/components/nav/nav.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import './nav.css'
+import {FaHome} from 'react-icons/fa'
+import {BiUser} from 'react-icons/bi'
+import {BiBook} from 'react-icons/bi'
+import {HiOutlineDesktopComputer} from 'react-icons/hi'
+import {MdOutlineMarkEmailUnread} from 'react-icons/md'
+
 
 
 function Nav() {
   return (
-    <div>nav</div>
+    <nav>
+      <a href="#header"><FaHome/></a>
+      <a href="#about"><BiUser/></a>
+      <a href="#experience"><BiBook/></a>
+      <a href="#portfolio"><HiOutlineDesktopComputer/></a>
+      <a href="#contact"><MdOutlineMarkEmailUnread/></a>
+    </nav>
   )
 }
 
