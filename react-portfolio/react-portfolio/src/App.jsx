669eb87250e88b1ffcace2eda538b3ff24e59cb8
Marquis <smcneal89@gmail.com>
2022-08-28 11:49:31 -0400

    added root variables, and set margin/padding for the page

--- react-portfolio/react-portfolio/src/App.jsx
+++ react-portfolio/react-portfolio/src/App.jsx
@@ -3,11 +3,7 @@ import Header from './components/header/Header'
 import Footer from './components/footer/Footer'
 import Nav from './components/nav/Nav'
 import About from './components/about/About'
-
-// Need to correct pathing
 import Experience from './components/experience/Experience'
-
-// Need to correct pathing
 import Portfolio from './components/portfolio/Portfolio'
 import Contact from './components/contact/Contact'
 
