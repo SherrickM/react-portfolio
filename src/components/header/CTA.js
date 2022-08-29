6db30ede6246c6cff30caba6040277104b256d2b
Marquis <smcneal89@gmail.com>
2022-08-29 01:10:15 -0400

    deploy

--- src/components/header/CTA.js
+++ src/components/header/CTA.js
@@ -4,8 +4,8 @@ import Resume from '../../assests/Resume.pdf'
 function CTA() {
   return (
     <div className='cta'>
-      <a href={Resume} download className='btn'> Resume</a>
-      <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+      <a href="https://drive.google.com/file/d/1UyeT-dYxEBQqu9qWycHxbJe4_sgKk3-w/view?usp=sharing" download className='btn'> Resume</a>
+      <a href="/contact" className='btn btn-primary'>Let's Talk</a>
     </div>
   )
 }
