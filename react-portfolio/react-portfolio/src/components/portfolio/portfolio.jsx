397a9cc67c6697f62098ef5181c32c335618c845
Marquis <smcneal89@gmail.com>
2022-08-28 21:20:29 -0400

    updated portfilio section jsx and css

--- react-portfolio/react-portfolio/src/components/portfolio/portfolio.jsx
+++ react-portfolio/react-portfolio/src/components/portfolio/portfolio.jsx
@@ -1,5 +1,15 @@
 import React from 'react'
 import './portfolio.css'
+import codeQuiz from '../../assests/codeQuiz.jpg'
+import HomeMade from '../../assests/HomeMade.jpg'
+import weatherApp from '../../assests/weatherApp.jpg'
+import NoteTaker from '../../assests/NoteTaker.jpg'
+import politeMail from '../../assests/politeMail.jpg'
+import SumItUp from '../../assests/SumItUp.jpg'
+import {FaGithub} from  'react-icons/fa'
+import {BiMoviePlay} from  'react-icons/bi'
+
+
 
 function Portfolio() {
   return (
@@ -11,50 +21,75 @@ function Portfolio() {
       <div className="container portfolio_container">
           {/* Portfolio Item 1 */}
           <div className="portfolio_item">
-              <div className="portfolio_item_img"></div>
+              <div className="portfolio_item_img">
+                  <img src={politeMail} className= "inner-img-style" alt="" />
+              </div>
               <h3>Polite-Mail</h3>
-              <a href="https://github.com/SherrickM/polite-mail" className='btn'>GitHub</a>
-              <a href="https://polite-mail.herokuapp.com/" className='btn btn-primary'>Live Demo</a>
+                <div className="portfolio-item-cta">
+                  <a href="https://github.com/SherrickM/polite-mail" className='about-icon'><FaGithub/></a>
+                  <a href="https://polite-mail.herokuapp.com/" className='about-icon'><BiMoviePlay/></a>
+                </div>
           </div>
 
           {/* Portfolio Item 2 */}
           <div className="portfolio_item">
-              <div className="portfolio_item_img"></div>
+              <div className="portfolio_item_img">
+                  <img src={SumItUp} className= "inner-img-style" alt="" />
+              </div>
               <h3>Sum It Up</h3>
-              <a href="https://github.com/SherrickM/sum-it-up" className='btn'>GitHub</a>
-              <a href="https://sum-it-up2.herokuapp.com/" className='btn btn-primary'>Live Demo</a>
+              <div className="portfolio-item-cta">
+
+                <a href="https://github.com/SherrickM/sum-it-up" className='about-icon'><FaGithub/></a>
+                <a href="https://sum-it-up2.herokuapp.com/" className='about-icon'><BiMoviePlay/></a>
+              </div>
           </div>
 
           {/* Portfolio Item 3 */}
           <div className="portfolio_item">
-              <div className="portfolio_item_img"></div>
+              <div className="portfolio_item_img">
+                  <img src={HomeMade} className= "inner-img-style" alt="" />
+              </div>
               <h3>HomeMade</h3>
-              <a href="https://github.com/SherrickM/homemade" className='btn'>GitHub</a>
-              <a href="https://sherrickm.github.io/homemade/" className='btn btn-primary'>Live Demo</a>
-          </div>
+                <div className="portfolio-item-cta">
+                  <a href="https://github.com/SherrickM/homemade" className='about-icon'><FaGithub/></a>
+                  <a href="https://sherrickm.github.io/homemade/" className='about-icon'><BiMoviePlay/></a>
+                </div>
+            </div>
 
           {/* Portfolio Item 4 */}
           <div className="portfolio_item">
-              <div className="portfolio_item_img"></div>
+              <div className="portfolio_item_img">
+                  <img src={codeQuiz} className= "inner-img-style" alt="" />
+              </div>
               <h3>Code Quiz</h3>
-              <a href="https://github.com/SherrickM/code-quiz-project" className='btn'>GitHub</a>
-              <a href="https://sherrickm.github.io/code-quiz-project/" className='btn btn-primary'>Live Demo</a>
+                <div className="portfolio-item-cta">
+                  <a href="https://github.com/SherrickM/code-quiz-project" className='about-icon'><FaGithub/></a>
+                  <a href="https://sherrickm.github.io/code-quiz-project/" className='about-icon'><BiMoviePlay/></a>
+                </div>
           </div>
 
           {/* Portfolio Item 5 */}
           <div className="portfolio_item">
-              <div className="portfolio_item_img"></div>
+              <div className="portfolio_item_img">
+                  <img src={weatherApp} className= "inner-img-style" alt="" />
+              </div>
               <h3>Weather Dashboard</h3>
-              <a href="https://github.com/SherrickM/weather-dashboard" className='btn'>GitHub</a>
-              <a href="https://sherrickm.github.io/weather-dashboard/" className='btn btn-primary'>Live Demo</a>
+                <div className="portfolio-item-cta">
+                  <a href="https://github.com/SherrickM/weather-dashboard" className='about-icon'><FaGithub/></a>
+                  <a href="https://sherrickm.github.io/weather-dashboard/" className='about-icon'><BiMoviePlay/></a>
+                </div>
           </div>
 
           {/* Portfolio Item 6 */}
           <div className="portfolio_item">
-              <div className="portfolio_item_img"></div>
+              <div className="portfolio_item_img">
+                  <img src={NoteTaker} className= "inner-img-style" alt="" />
+              </div>
               <h3>Note Taker</h3>
-              <a href="https://github.com/SherrickM/note-taker" className='btn'>GitHub</a>
-              <a href="https://guarded-springs-98042.herokuapp.com/" className='btn btn-primary'>Live Demo</a>
+                <div className="portfolio-item-cta">
+                  <a href="https://github.com/SherrickM/note-taker" className='about-icon'><FaGithub/></a>
+                  <a href="https://guarded-springs-98042.herokuapp.com/" className='about-icon'><BiMoviePlay/></a>
+                </div>
           </div>
 
       </div>
